5dada2177331d7e614e618661bc625c717aea701
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-09 10:11:46 -0500

    Initial Commit

---
+++ IconsAndSFSymbols/IconsAndSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconsAndSFSymbolsApp.swift
+//  IconsAndSFSymbols
+//
+//  Created by Teodoro Calle Lara on 9/02/24.
+//
+
+import SwiftUI
+
+@main
+struct IconsAndSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
