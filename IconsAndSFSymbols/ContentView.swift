4fef4424ff14c591b3c45c9f73f8673f1664eb2a
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-09 10:19:28 -0500

    First commit

--- IconsAndSFSymbols/ContentView.swift
+++ IconsAndSFSymbols/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hola a todos")
+            .underline()
+            .font(.largeTitle)
+            .bold()
+            .padding()
     }
 }
 
